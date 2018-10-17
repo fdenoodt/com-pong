e4566e727d3c1633b956abaceb70936cfc0b9538
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2018-10-17 17:50:37 +0200

    serverside user

--- server/scripts/user.js
+++ server/scripts/user.js
@@ -1,12 +1,58 @@
 class User {
-  constructor(socket, username, email, wins, losses, rankingPoints) {
-    this.socket = socket;
-    this.username = username;
-    this.email = email;
-    this.losses = losses;
-    this.wins = wins;
-    this.rankingPoints = rankingPoints;
+  constructor(socket, id, email, username, wins, losses, rankingpoints) {
+    this._socket = socket;
+    this._id = id;
+    this._username = username;
+    this._email = email;
+    this._losses = losses;
+    this._wins = wins;
+    this._rankingpoints = rankingpoints;
   }
+
+  get Id() {
+    return this._id;
+  }
+  get Username() {
+    return this._username;
+  }
+  get Email() {
+    return this._email;
+  }
+  get Losses() {
+    return this._losses;
+  }
+  get Wins() {
+    return this._wins;
+  }
+  get Rankingpoints() {
+    return this._rankingpoints;
+  }
+  get Socket() {
+    return this._socket;
+  }
+
+  set Id(value) {
+    this._id = value;
+  }
+  set Username(value) {
+    this._username = value;
+  }
+  set Email(value) {
+    this._email = value;
+  }
+  set Losses(value) {
+    this._losses = value;
+  }
+  set Wins(value) {
+    this._wins = value;
+  }
+  set Rankingpoints(value) {
+    this._rankingpoints = value;
+  }
+  set Socket(value) {
+    this._socket = value;
+  }
+
 }
 
 module.exports = User;
