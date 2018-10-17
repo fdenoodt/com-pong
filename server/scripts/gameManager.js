02c4ea31ece85f6afda8d34ee641168c9e6b1f37
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2018-10-17 22:19:22 +0200

    Game manager class

--- server/scripts/gameManager.js
+++ server/scripts/gameManager.js
@@ -1,7 +1,47 @@
+const User = require('./user.js');
+
 class GameManager {
   constructor() {
-    
+    this._lsGames = [];
+    this._lsSocketsInQueue = [];
   }
+
+  manageGames() {
+    for (let i = this._lsGames.length - 1; i >= 0; i--) {
+      this._lsGames[i].playTick();
+
+      if (ticks % 100 == 0) {
+        this._lsGames[i].afkTick();
+      }
+
+      if (this._lsGames[i].IsOver) {
+        this._lsGames.splice(i, 1); // Game deleted
+      }
+    }
+  }
+
+  createGame(duo) {
+    lsGames.push(new Game(duo));
+    console.log('creating game');
+  }
+
+  addPersonToQueue() {
+    this._lsSocketsInQueue.push(socket);
+    matchSockets();
+  }
+
+  matchSockets() {
+    const queueCount = this._lsSocketsInQueue.length;
+    if (queueCount > 1) {
+      for (let i = queueCount - 1; i > 0; i -= 2) {
+        const duo = [this._lsSocketsInQueue[i], this._lsSocketsInQueue[i - 1]]
+        createGame(duo);
+        this._lsSocketsInQueue.splice(i, 1);
+        this._lsSocketsInQueue.splice(i - 1, 1);
+      }
+    }
+  }
+
 }
 module.exports = GameManager;
 
