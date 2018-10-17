5bbcef54300d9f7209711811c37850da4ff5e21a
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2018-10-17 22:20:05 +0200

    move all game related things to gamemanager class.

--- server/server.js
+++ server/server.js
@@ -22,16 +22,7 @@ const gameManager = new GameManager();
 
 io.sockets.on('connection', newConnection);
 
-let lsGames = [];
-let lsSocketsInQueue = [];
-
 function newConnection(socket) {
-
-  function addPersonToQueue() {
-    lsSocketsInQueue.push(socket);
-    matchSockets();
-  }
-
   //TODO: FIND GAME TEMP DISABLED BECAUSE THE LOGIN MUST WORK FIRST
   // socket.on('findGame', () => {
   //   gameManager.addPersonToQueue(socke)
@@ -47,56 +38,12 @@ function newConnection(socket) {
 
 }
 
-// socket.emit('loginResponse', { isSuccessful: true, userData: user });
-// socket.emit('loginResponse', { isSuccessful: false });
-
-
-//TODO: this method should exist, but the loginResponse and registerResponse should be deleted
-function handleLogin(socket, user) {
-  activeUsers.push({ socket, user })
-  socket.emit('accountStateChange', true);
-}
-
-//to log out: firebase.auth().signOut();
-
 const timerTick = () => {
   ticks++;
-  manageGames();
+  if (ticks % 1000 == 0)
+    userManager.manageUsers();
+    gameManager.manageGames();
 }
-
-const matchSockets = () => {
-  const queueCount = lsSocketsInQueue.length;
-  if (queueCount > 1) {
-    for (let i = queueCount - 1; i > 0; i -= 2) {
-      const duo = [lsSocketsInQueue[i], lsSocketsInQueue[i - 1]]
-      createGame(duo);
-      lsSocketsInQueue.splice(i, 1);
-      lsSocketsInQueue.splice(i - 1, 1);
-    }
-  }
-}
-
-const createGame = (duo) => {
-  lsGames.push(new Game(duo));
-  console.log('creating game');
-
-}
-
-const manageGames = () => {
-  for (let i = lsGames.length - 1; i >= 0; i--) {
-    lsGames[i].playTick();
-
-    if (ticks % 100 == 0) {
-      lsGames[i].afkTick();
-    }
-
-    if (lsGames[i].IsOver) {
-      lsGames.splice(i, 1);
-      // Game deleted'
-    }
-  }
-}
-
 
 let ticks = 0;
 const timer = setInterval(timerTick, 6);
