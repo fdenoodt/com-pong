c61e5f9a263b01c1af0e9531974b9e220e87da38
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2018-10-31 20:12:33 +0100

    display top 3 players in table.

--- www/js/app/scoreboard.js
+++ www/js/app/scoreboard.js
@@ -9,14 +9,32 @@ const initScoreboard = () => {
 
 }
 
-const handleScoreboardResponse = (data) => {
-  console.log(data);
+const handleScoreboardResponse = (res) => {
+  const data = res.data;
+
   if (data.isSuccessful) {
     const lsUsers = data.result;
+    const table = document.querySelector('.tblWins');
+    let rankState = 1;
     for (const user of lsUsers) {
+      const row = table.insertRow(table.rows.length);
 
+      const rowRank = row.insertCell(0);
+      const rowUsername = row.insertCell(1);
+      const rowWins = row.insertCell(2);
+      const rowLosses = row.insertCell(3);
+      const rowRatio = row.insertCell(4);
+
+      const username = user.username;
+      const wins = user.wins;
+      const losses = user.losses;
+      const ratio = wins / losses == Infinity || (wins == 0 && losses == 0) ? '-' : wins / losses;
+      rowRank.innerHTML = rankState++;
+      rowUsername.innerHTML = username;
+      rowWins.innerHTML = wins;
+      rowLosses.innerHTML = losses;
+      rowRatio.innerHTML = ratio;
     }
-    //tblWins
   }
   else {
     warn('Warning', 'Something went wrong.. try again later.');
